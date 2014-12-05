eaf0fad3b6050b8626cc772e91688c100d2e3dba
ahardstylec <andreas.collmann@gmail.com>
2014-12-05 15:48:36 +0100

    better code quality

--- benchmark.rb
+++ benchmark.rb
@@ -5,8 +5,12 @@ if `echo $PATH | grep mpich` && ! $?.success?
   `export export PATH=/home/mpich/bin:$PATH && make clean && make`
 end
 
+FileUtils.mkdir_p "benchmarks"
+
+puts "calc matrices parallel"
 #For other matrix sizes
 counter = 10
+File.open("benchmarks/matrices_parallel.txt", 'w') do |f|
 10.times do |i|
 	system("./matrix_generator #{counter} #{counter} > matrix#{counter}.txt")
 	puts "Benchmark for #{counter}"
@@ -18,14 +22,17 @@ counter = 10
 			exit 1
 		end
 		ende = (Time.now - start)
-		puts "#{k}\t #{ende}"
+		f.puts "#{k}\t #{ende}"
 	end
 	counter += 10
 end
 
-(1..100).each do |i|
-	start = Time.now
-	system("mpiexec  -n #{i} ./matrix_parallel matrix-A-10x10.txt matrix-B-10x10.txt > /dev/null")
-	ende = (Time.now - start)
-	puts "#{i}\t #{ende}"
+puts "calc matrices sequential"
+File.open("benchmarks/matrices_sequential.txt", 'w') do |f|
+	(1..100).each do |i|
+		start = Time.now
+		system("./matrix_sequential matrix-A-10x10.txt matrix-B-10x10.txt > /dev/null")
+		ende = (Time.now - start)
+		f.puts "#{i}\t #{ende}"
+	end
 end
