e262a957375d0e7497752373173dd721cb06ae37
ahardstylec <andreas.collmann@gmail.com>
2014-12-05 16:12:58 +0100

    better code quality

---
+++ benchmark_sequential.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+# check if path to mpiexec is set, if not set  path and recompile binaries
+if `echo $PATH | grep mpich` && ! $?.success?
+  `export export PATH=/home/mpich/bin:$PATH && make clean && make`
+end
+
+FileUtils.mkdir_p("benchmarks")
+
+puts "calc matrices sequential"
+counter = 10
+File.open("benchmarks/matrices_sequential.txt", 'w') do |f|
+	(1..100).each do |i|
+		system("./matrix_generator #{counter} #{counter} > matrix#{counter}.txt")
+		start = Time.now
+		system("./matrix_sequential matrix#{counter}.txt matrix#{counter}.txt > /dev/null")
+		ende = (Time.now - start)
+		f.puts "#{i}\t #{ende}"
+		counter += 10
+	end
+end
