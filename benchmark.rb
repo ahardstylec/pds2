8e8230b7d0eff4576ff4551f656b4078ddc50b34
ahardstylec <andreas.collmann@gmail.com>
2014-12-05 14:46:23 +0100

    better code quality

--- benchmark.rb
+++ benchmark.rb
@@ -8,10 +8,9 @@ end
 #For other matrix sizes
 counter = 10
 10.times do |i|
-	counter += 10
 	system("./matrix_generator #{counter} #{counter} > matrix#{counter}.txt")
 	puts "Benchmark for #{counter}"
-	100.times do |k|
+	(1..100).each do |k|
 		start = Time.now
 		success = system("mpiexec  -n #{k} ./matrix_parallel matrix#{counter}.txt matrix#{counter}.txt > /dev/null")
 		unless success
@@ -21,9 +20,10 @@ counter = 10
 		ende = (Time.now - start)
 		puts "#{k}\t #{ende}"
 	end
+	counter += 10
 end
 
-100.times do |i|
+(1..100).each do |i|
 	start = Time.now
 	system("mpiexec  -n #{i} ./matrix_parallel matrix-A-10x10.txt matrix-B-10x10.txt > /dev/null")
 	ende = (Time.now - start)
