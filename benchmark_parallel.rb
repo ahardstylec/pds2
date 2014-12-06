5d982833b5bdf87c80b5e189014d3374321f0a72
ahardstylec <andreas.collmann@gmail.com>
2014-12-06 10:50:18 +0100

    better code quality

--- benchmark_parallel.rb
+++ benchmark_parallel.rb
@@ -14,17 +14,15 @@ counter = 10
 File.open("benchmarks/matrices_parallel.txt", 'w') do |f|
 	f.sync=true
 	10.times do |i|
-		proc_counter_add=2
 		system("./matrix_generator #{counter} #{counter} > matrix#{counter}.txt")
 		puts "Benchmark for #{counter}"
-		(1..20).each do |k|
-			out = `mpiexec  -n #{proc_counter_add} ./matrix_parallel matrix#{counter}.txt matrix#{counter}.txt`
+		(1..50).each do |k|
+			out = `mpiexec  -n #{k} ./matrix_parallel matrix#{counter}.txt matrix#{counter}.txt`
 			unless $?
 				puts "Some Error occurd during mpiexec with #{k} processes on matrix: matrix#{counter}.txt"
 				exit 1
 			end
 			f.write out
-			proc_counter_add=5*k
 		end
 		counter += 100
 	end
