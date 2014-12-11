1c2bfe6928d73392a70b34ec54fe415142e64e59
ahardstylec <andreas.collmann@gmail.com>
2014-12-11 08:42:41 +0100

    add hostfile ident. in matrix parallel

--- matrix_parallel.cpp
+++ matrix_parallel.cpp
@@ -53,10 +53,15 @@ int main(int argc, char *argv[]) {
     }
 
     double startwtime = 0.0, endwtime;
-    int myid, numprocs;
+    int myid, numprocs, hostname_len;
+
+
+    char hostname[20];
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+
+    MPI_Get_processor_name(hostname, &hostname_len);
 
     if (myid == 0)
         startwtime = MPI_Wtime();
