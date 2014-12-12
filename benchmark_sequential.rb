abd898f45b63a96a40cdab8da0382b5145e9e14c
ahardstylec <andreas.collmann@gmail.com>
2014-12-12 11:58:48 +0100

    changed benchmark

--- benchmark_sequential.rb
+++ benchmark_sequential.rb
@@ -10,9 +10,9 @@ FileUtils.mkdir_p("benchmarks")
 puts "calc matrices sequential"
 counter = 10
 File.open("benchmarks/matrices_sequential.txt", 'w') do |f|
-	(1..10).each do |i|
+	(1..1000).each do |i|
 		system("./matrix_generator #{counter} #{counter} > matrix#{counter}.txt")
 		f.puts `mpiexec -n 1 ./matrix_sequential matrix#{counter}.txt matrix#{counter}.txt`
-		counter += 100
+		counter += 10
 	end
 end
